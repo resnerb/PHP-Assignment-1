452b02ddf7c94e09a605d3b582be4d0ecf8e43b5
resnerb <resnerb@onid.oregonstate.edu>
2015-05-02 23:58:39 -0700

    Working on login.php and content1.php

--- src/content1.php
+++ src/content1.php
@@ -1,1 +1,23 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<title>GET/POST Data</title>
+</head>
+<body>
+<?php
+    
+    $username = $_POST["username"];
+    echo $username;
+    if ($username === '' || !isset($username))
+    {
+        echo 'A username must be entered. Click <a href="login.php">here</a> to return to the login screen.';
+    }
+    else
+    {
+        echo 'Hello ' . $username . ' you have visited this site ' . $numVisits . ' times before. Click here to logout.';
+    }
+    
+?>
+</body>
+</html>
--- src/login.php
+++ src/login.php
@@ -1,1 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<title>Login Page</title>
+</head>
+<body>
 
+<form action="content1.php" method="post">
+Username: <input type="text" name="username"><br>
+<input type="submit" value="Login">
+</form>
+
+</body>
+</html>
